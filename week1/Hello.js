ef9b8d503c7e309e0824da07b8ad3b6042b1d663
hodan97 <83996550+hodan97@users.noreply.github.com>
2021-06-07 12:44:23 +0100

    update

---
+++ week1/Hello.js
@@ -0,0 +1,31 @@
+console.log ("hello");
+console.log ("hello".length);
+console.log ("hello".toUpperCase());
+console.log(Math.random()*10); 
+console.log(Math.floor(Math.random()*10));
+console.log("    |    |    ");
+console.log("    |    |    ");
+console.log("    |    |    ");
+console.log("--------------");
+console.log("    |    |    ");
+console.log("    |    |    ");
+console.log("    |    |    ");
+console.log("--------------");
+console.log("    |    |    ");
+console.log("    |    |    ");
+console.log("    |    |    ");
+let grid = [" | | "," | | "," | | ","-----------"," | | "," | | "," | | ","-----------"," | | "," | | "," | | "];
+let q = 0;
+do {
+    console.log(grid[q]);
+    q++
+}
+while(q<11);
+console.log ("All Around the world".charAt(7).toUpperCase());
+console.log ("All Around the world".toUpperCase().charAt(7));
+// console.log
+let favouriteDrink ="Tea";
+console.log(favouriteDrink);
+console.log ("My favourite drink is " + favouriteDrink);
+console.log (`My favourite drink is ${favouriteDrink}`);
+console.log (`My favourite drink is now ${favouriteDrink}`);
---
+++ week1/thursday.js
@@ -0,0 +1,4 @@
+let music= "classical";
+if (music == "classical"){
+    console.log("Oh no it's that classical again"):
+}
